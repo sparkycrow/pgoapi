fd5d308e63ad444da68fc8d45362c53badd360d3
sparkycrow <31147831+sparkycrow@users.noreply.github.com>
2017-12-01 01:09:21 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author='tjado',
     description='Pokemon Go API lib',
     version='1.2.0',
-    url='https://github.com/sebastienvercammen/pgoapi',
-    download_url="https://github.com/sebastienvercammen/pgoapi/releases",
+    url='https://github.com/sparkycrow/pgoapi',
+    download_url="https://github.com/sparkycrow/pgoapi/releases",
     packages=find_packages(),
     install_requires=reqs)
